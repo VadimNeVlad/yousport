4515f93ce84029d62f1be597a5e84c0f812515f0
Vadym Marukhnenko <vdm.marukhnenko@gmail.com>
2023-08-29 12:16:03 +0100

    finished meals module

--- src/app/meals/new-meal/new-meal.component.ts
+++ src/app/meals/new-meal/new-meal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { Meal } from 'src/app/shared/models/meal';
 import { MealsService } from 'src/app/shared/services/meals.service';
 
@@ -8,19 +9,49 @@ import { MealsService } from 'src/app/shared/services/meals.service';
   templateUrl: './new-meal.component.html',
   styleUrls: ['./new-meal.component.scss'],
 })
-export class NewMealComponent implements OnDestroy {
+export class NewMealComponent implements OnInit, OnDestroy {
+  mealId = '';
+
+  meal$ = new Observable<Meal>();
   unsubscribe$ = new Subject<void>();
 
-  constructor(private mealService: MealsService) {}
+  constructor(
+    private mealService: MealsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
 
+  ngOnInit(): void {
+    this.mealId = this.route.snapshot.paramMap.get('id') || '';
+
+    if (this.mealId) this.getMeal();
+  }
+
+  getMeal(): void {
+    this.meal$ = this.mealService.getMeal(this.mealId);
+  }
+
   addMeal(meal: Meal): void {
     this.mealService
       .addMeal(meal)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+  }
+
+  updateMeal(meal: Meal): void {
+    this.mealService
+      .updateMeal(meal)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+  }
+
+  deleteMeal(mealId: number): void {
+    this.mealService
+      .deleteMeal(mealId)
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe();
   }
